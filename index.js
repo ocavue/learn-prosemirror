40f8b8b3a816b4d8c1045db154064aa0a8c428dd
ocavue <ocavue@MBP.no_rep>
2018-11-07 20:14:27 +0800

    Transactions

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ import { EditorState } from "prosemirror-state"
 import { EditorView } from "prosemirror-view"
 
 let state = EditorState.create({ schema })
-let view = new EditorView(document.querySelector('#editor'), { state })
+let view = new EditorView(document.querySelector('#editor'), {
+  state,
+  dispatchTransaction(transaction) {
+    console.log("Document size went from", transaction.before.content.size,
+                "to", transaction.doc.content.size)
+    let newState = view.state.apply(transaction)
+    view.updateState(newState)
+  }
+})
