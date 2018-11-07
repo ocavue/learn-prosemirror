ade2ef7db2f677ca433e38dbf74cc70d37790df5
ocavue <ocavue@MBP.no_rep>
2018-11-07 20:13:32 +0800

    My first editor

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const {EditorState} = require("prosemirror-state")
-const {EditorView} = require("prosemirror-view")
-const {Schema, DOMParser} = require("prosemirror-model")
-const {schema} = require("prosemirror-schema-basic")
-const {addListNodes} = require("prosemirror-schema-list")
-const {exampleSetup} = require("prosemirror-example-setup")
+import { schema } from "prosemirror-schema-basic"
+import { EditorState } from "prosemirror-state"
+import { EditorView } from "prosemirror-view"
 
-// Mix the nodes from prosemirror-schema-list into the basic schema to
-// create a schema with list support.
-const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks
-})
-
-window.view = new EditorView(document.querySelector("#editor"), {
-  state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
-    plugins: exampleSetup({schema: mySchema})
-  })
-})
+let state = EditorState.create({ schema })
+let view = new EditorView(document.querySelector('#editor'), { state })
