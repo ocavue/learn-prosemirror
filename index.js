9449efb382b7fb029c555896618f80fa91798010
ocavue <ocavue@MBP.no_rep>
2018-11-07 20:15:40 +0800

    Plugins

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import { schema } from "prosemirror-schema-basic"
 import { EditorState } from "prosemirror-state"
 import { EditorView } from "prosemirror-view"
+import { undo, redo, history } from "prosemirror-history"
+import { keymap } from "prosemirror-keymap"
 
-let state = EditorState.create({ schema })
+let state = EditorState.create({
+  schema,
+  plugins: [
+    history(),
+    keymap({ "Mod-z": undo, "Mod-y": redo })
+  ]
+})
 let view = new EditorView(document.querySelector('#editor'), {
   state,
-  dispatchTransaction(transaction) {
-    console.log("Document size went from", transaction.before.content.size,
-                "to", transaction.doc.content.size)
+  dispatchTransaction (transaction) {
+    console.log(
+      "Document size went from", transaction.before.content.size,
+      "to", transaction.doc.content.size
+    )
     let newState = view.state.apply(transaction)
     view.updateState(newState)
   }
